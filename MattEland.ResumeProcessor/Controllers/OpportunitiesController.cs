20d6c9333dffc8da10fdc8855180b60fe98e3da1
Matt Eland <Matt.Eland@GMail.com>
2020-04-04 00:42:13 -0400

    Listing items is now a possibility

--- MattEland.ResumeProcessor/Controllers/OpportunitiesController.cs
+++ MattEland.ResumeProcessor/Controllers/OpportunitiesController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using MattEland.ResumeProcessor.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MattEland.ResumeProcessor.Controllers
 {
@@ -14,7 +15,23 @@ namespace MattEland.ResumeProcessor.Controllers
         {
             using (var context = new ResumeContext())
             {
-                return context.Opportunities.ToList();
+                // Add a sample opportunity if none is present
+                if (!context.Opportunities.Any())
+                {
+                    context.Opportunities.Add(new Opportunity()
+                    {
+                        Company = "FakeCorp",
+                        PostalCode = "43081",
+                        JobTitle = "Basket Engineer",
+                        DesiredSkills = new List<Skill>
+                        {
+                            new Skill { ShortName = "Underwater Basket Weaving", Level = SkillLevel.Beginner }
+                        }
+                    });
+                    context.SaveChanges();
+                }
+
+                return context.Opportunities.Include(o => o.DesiredSkills).ToList();
             }
         }
     }
