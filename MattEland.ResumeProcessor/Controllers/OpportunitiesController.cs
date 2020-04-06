7e3ed87483fded229802fbbf49e37e123435df05
Matt Eland <Matt.Eland@GMail.com>
2020-04-05 23:10:03 -0400

    Update opportunity

--- MattEland.ResumeProcessor/Controllers/OpportunitiesController.cs
+++ MattEland.ResumeProcessor/Controllers/OpportunitiesController.cs
@@ -40,6 +40,41 @@ namespace MattEland.ResumeProcessor.Controllers
             return context.Opportunities.Include(o => o.DesiredSkills).FirstOrDefault(o => o.Id == id);
         }
 
+        [HttpPost]
+        public ActionResult<Opportunity> CreateOpportunity(Opportunity opportunity)
+        {
+            if (opportunity == null) return new BadRequestResult();
+
+            using var context = new ResumeContext();
+
+            var result = context.Opportunities.Add(opportunity);
+            context.SaveChanges();
+
+            // TODO: This really should return a full URL starting relative to the controller
+            return Created($"/api/opportunities/{result.Entity.Id}", result.Entity);
+        }
+
+        [HttpPut("{id:int}")]
+        [HttpPost("{id:int}")]
+        public ActionResult<Opportunity> UpdateOpportunity(int id, Opportunity opportunity)
+        {
+            if (opportunity == null || id != opportunity.Id) return new BadRequestResult();
+            
+            using var context = new ResumeContext();
+
+            var existing = GetOppById(id, context);
+            if (existing == null) return NotFound("No opportunity exists with that ID or you do not have access to it.");
+
+            existing.Company = opportunity.Company;
+            existing.JobTitle = opportunity.JobTitle;
+            existing.PostalCode = opportunity.PostalCode;
+
+            // TODO: This should also update desired skills, potentially
+            context.SaveChanges();
+
+            return Ok(existing);
+        }
+
         [HttpDelete("{id:int}")]
         public ActionResult<Opportunity> DeleteOpportunity(int id)
         {
@@ -58,7 +93,6 @@ namespace MattEland.ResumeProcessor.Controllers
 
             return NotFound("No opportunity exists with that ID or you do not have access to it.");
         }
-
 
         private static void InitOpportunitiesIfNeeded(ResumeContext context)
         {
